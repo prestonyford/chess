e2f1ec57a48bc90c83699cc7333d552a2d01ed75
Preston Ford <preston.y.ford@gmail.com>
2024-01-24 22:15:22 -0700

    makeMove, validMoves, isInCheck refactor

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -57,7 +58,28 @@ public class ChessGame {
         if (piece == null) {
             return null;
         }
-        return piece.pieceMoves(this.chessBoard, startPosition);
+        Collection<ChessMove> moves = piece.pieceMoves(this.chessBoard, startPosition);
+        HashSet<ChessMove> legalMoves = new HashSet<>();
+
+        for (ChessMove move: moves) {
+            ChessPiece startPositionPiece = chessBoard.getPiece(move.getStartPosition());
+            ChessPiece endPositionPiece = chessBoard.getPiece(move.getEndPosition());
+
+            // Make the move but don't promote yet
+            chessBoard.addPiece(startPosition, null);
+            chessBoard.addPiece(move.getEndPosition(), piece);
+
+            // If still not in check, add to legalMoves
+            if (!isInCheck(this.teamTurn)) {
+                legalMoves.add(move);
+            }
+
+            // Revert the move
+            chessBoard.addPiece(startPosition, startPositionPiece);
+            chessBoard.addPiece(move.getEndPosition(), endPositionPiece);
+        }
+
+        return legalMoves;
     }
 
     /**
@@ -68,6 +90,7 @@ public class ChessGame {
      */
     public void makeMove(ChessMove move) throws InvalidMoveException {
         ChessPosition startPosition = move.getStartPosition();
+        ChessPosition endPosition = move.getEndPosition();
         ChessPiece piece = chessBoard.getPiece(startPosition);
         Collection<ChessMove> moves = validMoves(startPosition);
 
@@ -75,21 +98,11 @@ public class ChessGame {
                 moves != null && moves.contains(move) &&
                 piece.getTeamColor() == this.teamTurn
         ) {
-            // Make the move but don't promote yet
-            ChessPosition endPosition = move.getEndPosition();
-            ChessPiece pieceAtEndPosition = chessBoard.getPiece(endPosition);
             chessBoard.addPiece(startPosition, null);
             chessBoard.addPiece(endPosition, piece);
 
-            // If now in check, revert move and throw illegal move. Icky I know but I can't change function signature
-            if (isInCheck(this.teamTurn)) {
-                chessBoard.addPiece(startPosition, piece);
-                chessBoard.addPiece(endPosition, pieceAtEndPosition);
-                throw new InvalidMoveException();
-            }
-
-            // If the move was kept (not reverted), check for promotion and promote accordingly
-            else if (move.getPromotionPiece() != null) {
+            // Check for promotion and promote accordingly
+            if (move.getPromotionPiece() != null) {
                 chessBoard.addPiece(endPosition, new ChessPiece(this.teamTurn, move.getPromotionPiece()));
             }
 
@@ -117,10 +130,12 @@ public class ChessGame {
         for (int row = 1; row <= 8; ++row) {
             for (int col = 1; col <= 8; ++col) {
                 ChessPosition startPosition = new ChessPosition(row, col);
-                Collection<ChessMove> pieceMoves = validMoves(startPosition);
-                if (pieceMoves == null) {
+                ChessPiece piece = chessBoard.getPiece(startPosition);
+                if (piece == null) {
                     continue;
                 }
+                Collection<ChessMove> pieceMoves = piece.pieceMoves(chessBoard, startPosition);
+
                 TeamColor pieceColor = chessBoard.getPiece(startPosition).getTeamColor();
 
                 for (ChessMove move: pieceMoves) {
@@ -190,48 +205,37 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        // if the king is already in check, it is in stalemate if there are no moves
+        HashSet<ChessMove> allyMoves = new HashSet<>();
 
-        // ChessGame copyGame = new ChessGame(this);
-        if (!isInCheck(teamColor)) {
-            for (int row = 1; row <= 8; ++row) {
-                for (int col = 1; col <= 8; ++col) {
-                    ChessPosition position = new ChessPosition(row, col);
-                    ChessPiece piece = chessBoard.getPiece(position);
-                    if (piece == null || piece.getTeamColor() != teamColor) {
-                        continue;
-                    }
-                    Collection<ChessMove> pieceMoves = validMoves(position);
-                    if (pieceMoves == null) {
-                        continue;
-                    }
-
-                    for (ChessMove move: pieceMoves) {
-                        try {
-                            ChessPiece startPosPiece = this.getBoard().getPiece(move.getStartPosition());
-                            ChessPiece finalPosPiece = this.getBoard().getPiece(move.getEndPosition());
-                            this.makeMove(move);
-                            if (!this.isInCheck(teamColor)) {
-                                // revert move
-                                this.getBoard().addPiece(move.getStartPosition(), startPosPiece);
-                                this.getBoard().addPiece(move.getEndPosition(), finalPosPiece);
-                                return false;
-                            }
-                            // revert move
-                            this.getBoard().addPiece(move.getStartPosition(), startPosPiece);
-                            this.getBoard().addPiece(move.getEndPosition(), finalPosPiece);
-                        }
-                        catch (InvalidMoveException ex) {
-                            continue;
-                        }
-                    }
+        for (int row = 1; row <= 8; ++row) {
+            for (int col = 1; col <= 8; ++col) {
+                ChessPosition position = new ChessPosition(row, col);
+                ChessPiece piece = chessBoard.getPiece(position);
+                if (piece == null || piece.getTeamColor() != teamColor) {
+                    continue;
+                }
+                Collection<ChessMove> pieceMoves = validMoves(position);
+                if (pieceMoves != null) {
+                    allyMoves.addAll(pieceMoves);
                 }
             }
         }
-        else {
-            System.out.println("here!");
-        }
 
+        for (ChessMove move: allyMoves) {
+            ChessPiece startPositionPiece = chessBoard.getPiece(move.getStartPosition());
+            ChessPiece endPositionPiece = chessBoard.getPiece(move.getEndPosition());
+            try {
+                this.makeMove(move);
+                // If it made it here it is valid
+                // revert the move if it was legal
+                chessBoard.addPiece(move.getStartPosition(), startPositionPiece);
+                chessBoard.addPiece(move.getEndPosition(), endPositionPiece);
+                return false;
+            }
+            catch (Exception ex) {
+                continue;
+            }
+        }
         return true;
     }
 
