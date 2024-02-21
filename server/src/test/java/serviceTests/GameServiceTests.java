5c4dc0cd968968c3d9d1ddb2c5f9ab0454c8e5c2
Preston Ford <preston.y.ford@gmail.com>
2024-02-21 16:50:13 -0700

    join game unit tests

--- server/src/test/java/serviceTests/GameServiceTests.java
+++ server/src/test/java/serviceTests/GameServiceTests.java
@@ -1,8 +1,10 @@
 package serviceTests;
 
 import chess.dataModel.request.CreateGameRequest;
+import chess.dataModel.request.JoinGameRequest;
 import chess.dataModel.request.LoginRequest;
 import chess.dataModel.request.RegisterRequest;
+import chess.dataModel.response.CreateGameResponse;
 import chess.dataModel.response.LoginResponse;
 import chess.dataModel.response.RegisterResponse;
 import org.junit.jupiter.api.*;
@@ -15,14 +17,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class GameServiceTests {
-    private final UserService userService = UserService.getInstance();
-    private final GameService gameService = GameService.getInstance();
+    private static final UserService userService = UserService.getInstance();
+    private static final GameService gameService = GameService.getInstance();
+    private static RegisterResponse registerResponse;
+    private static CreateGameResponse createGameResponse;
 
-    @Test
-    @Order(1)
-    @DisplayName("Create game")
-    public void createGame() throws TestException {
-        RegisterResponse registerResponse;
+    @BeforeAll
+    public static void init() {
         try {
             registerResponse = userService.register(new RegisterRequest(
                     "Steve",
@@ -32,9 +33,14 @@ public class GameServiceTests {
         } catch (Exception ex) {
             throw new TestException("Could not create an account");
         }
+    }
 
+    @Test
+    @Order(1)
+    @DisplayName("Create game")
+    public void createGame() throws TestException {
         try {
-            gameService.createGame(registerResponse.authToken(), new CreateGameRequest("gameA"));
+            createGameResponse = gameService.createGame(registerResponse.authToken(), new CreateGameRequest("gameA"));
         } catch (Exception ex) {
             throw new TestException("Could not create a game");
         }
@@ -44,20 +50,58 @@ public class GameServiceTests {
     @Order(2)
     @DisplayName("Create game with bad name")
     public void badCreateGame() throws TestException {
-        LoginResponse loginResponse;
+        assertThrows(
+                ServiceException.class,
+                () -> gameService.createGame(registerResponse.authToken(), new CreateGameRequest("")),
+                "Service allowed an empty string game name when it shouldn't have"
+        );
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Join game")
+    public void joinGame() throws TestException {
         try {
-            loginResponse = userService.login(new LoginRequest(
-                    "Steve",
-                    "Steve's password"
-            ));
+            gameService.joinGame(
+                    registerResponse.authToken(),
+                    new JoinGameRequest("WHITE", createGameResponse.gameID())
+            );
         } catch (Exception ex) {
-            throw new TestException("Could not login to account");
+            throw new TestException("Could not join game");
+        }
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Attempt to join game as white that already has white")
+    public void badJoinGame() throws TestException {
+        RegisterResponse registerResponsePlayer2;
+        try {
+            registerResponsePlayer2 = userService.register(
+                    new RegisterRequest(
+                            "Player2",
+                            "player2password",
+                            "player2@email.com"
+                    )
+            );
+        } catch (Exception ex) {
+            throw new TestException("Could not create player 2 account");
         }
 
         assertThrows(
                 ServiceException.class,
-                () -> gameService.createGame(loginResponse.authToken(), new CreateGameRequest("")),
-                "Service allowed an empty string game name when it shouldn't have"
+                () -> gameService.joinGame(
+                        registerResponsePlayer2.authToken(),
+                        new JoinGameRequest("WHITE", createGameResponse.gameID())
+                ),
+                "Game service allowed a player to join white when there was already a white player"
         );
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("List games")
+    public void listGames() throws TestException {
+        
     }
 }
