82d68c21611d50068bc537467bcf6ce5bba5f3b7
Preston Ford <preston.y.ford@gmail.com>
2024-03-02 14:09:23 -0700

    clear parameterized unit test refactor

--- server/src/test/java/dataAccess/DataAccessTest.java
+++ server/src/test/java/dataAccess/DataAccessTest.java
@@ -1,9 +1,14 @@
 package dataAccess;
 
+import chess.dataModel.UserData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import service.exceptions.ServiceException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class DataAccessTest {
     private DataAccess getDataAccess(Class<? extends DataAccess> databaseClass) throws DataAccessException {
@@ -19,8 +24,17 @@ public class DataAccessTest {
 
     @ParameterizedTest
     @ValueSource(classes = {MemoryDataAccess.class, SQLDataAccess.class})
-    public void clearDB(Class<? extends DataAccess> dbClass) throws DataAccessException {
+    public void clear(Class<? extends DataAccess> dbClass) throws DataAccessException {
         DataAccess dataAccess = getDataAccess(dbClass);
+
+        // Create a user
+        dataAccess.createUser(new UserData(
+                "Navia",
+                "Navia's password",
+                "Navia@email.com"
+        ));
+        assertNotNull(dataAccess.getUser("Navia"), "Could not add a user to the db");
         dataAccess.clear();
+        assertNull(dataAccess.getUser("Navia"), "User was still in db after clear");
     }
 }
